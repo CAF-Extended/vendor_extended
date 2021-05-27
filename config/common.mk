AospExtended/platform_vendor_aosp

aosp: Inherit makefiles of gapps
Signed-off-by: sb6596 <shubhamprince111@gmail.com>
Change-Id: I72cf87e395b0e4ce5a47019a4a9e8a98ac752e57

config/common.mk
@@ -14,7 +14,7 @@
 
 # Gapps
 ifeq ($(WITH_GAPPS),true)
-include vendor/aosp/config/gapps.mk
+$(call inherit-product, vendor/aosp/config/gapps.mk)
 endif
 
 include vendor/aosp/config/version.mk

CAF-Extended/vendor_extended

config/common.mk
@@ -16,7 +16,7 @@ include vendor/extended/config/version.mk
 
 # Gapps
 ifeq ($(WITH_GAPPS),true)
-include vendor/extended/config/gapps.mk
+$(call inherit-product, vendor/extended/config/gapps.mk)
 endif
 
 # Backup Tool