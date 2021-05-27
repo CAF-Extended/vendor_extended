AospExtended/platform_vendor_aosp

config: Add back support for gapps and non-gapps build
Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
Change-Id: I99ad9ce736dd22ee0a0893f8fbc6fe4307376804

aosp: Import overlays from PixelExperience and refactor for AEX

Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
Change-Id: I4ffaf278570661d2da7d40f883315f30c72115fc

config/common.mk
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Gapps
+ifeq ($(WITH_GAPPS),true)
+include vendor/aosp/config/gapps.mk
+endif
+
 include vendor/aosp/config/version.mk
 
 # Audio files
@@ -40,12 +45,12 @@ endif
 $(call inherit-product, vendor/aosp/config/bootanimation.mk)
 
 # Common Overlay
-PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/aosp/overlay/common
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay/common 
 
 # Exclude RRO Enforcement
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
-    vendor/aosp/overlay/common
+    vendor/aosp/overlay
 
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED ?= true

config/gapps.mk
@@ -1,0 +1,13 @@
+# Gapps
+include vendor/gapps/config.mk
+IS_PHONE := true
+
+# Common Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay-gapps/common
+
+# Exclude RRO Enforcement
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+    vendor/aosp/overlay-gapps
+    
+$(call inherit-product, vendor/aosp/config/rro_overlays.mk)

CAF-Extended/vendor_extended

config/common.mk
@@ -14,6 +14,11 @@
 
 include vendor/extended/config/version.mk
 
+# Gapps
+ifeq ($(WITH_GAPPS),true)
+include vendor/extended/config/gapps.mk
+endif
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/extended/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
@@ -26,7 +31,6 @@ PRODUCT_COPY_FILES += \
     vendor/extended/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
     vendor/extended/prebuilt/common/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
 endif
-
 
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED ?= true

config/gapps.mk
@@ -1,0 +1,13 @@
+# Gapps
+include vendor/gapps/config.mk
+IS_PHONE := true
+
+# Common Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/extended/overlay-gapps/common
+
+# Exclude RRO Enforcement
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+    vendor/extended/overlay-gapps
+    
+$(call inherit-product, vendor/extended/config/rro_overlays.mk)