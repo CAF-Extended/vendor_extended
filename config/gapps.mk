AospExtended/platform_vendor_aosp

config: Add back support for gapps and non-gapps build
Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
Change-Id: I99ad9ce736dd22ee0a0893f8fbc6fe4307376804

aosp: Import overlays from PixelExperience and refactor for AEX

Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
Change-Id: I4ffaf278570661d2da7d40f883315f30c72115fc

config/gapps.mk
@@ -1,0 +1,13 @@
+# Gapps
+include vendor/gapps/config.mk
+IS_PHONE := true
+
+# Common Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay-gapps/common
+
+# Exclude RRO Enforcement
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+    vendor/aosp/overlay-gapps
+    
+$(call inherit-product, vendor/aosp/config/rro_overlays.mk)

CAF-Extended/vendor_extended

config/gapps.mk
@@ -1,0 +1,12 @@
+# Gapps
+IS_PHONE := true
+
+# Common Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/extended/overlay-gapps/common
+
+# Exclude RRO Enforcement
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+    vendor/extended/overlay-gapps
+    
+$(call inherit-product, vendor/extended/config/rro_overlays.mk)